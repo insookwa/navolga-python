01ba7a6fe8db5a6be1e0b42741ea7d8a26450424
insookwa <nsookwaivan@gmail.com>
2025-02-02 11:35:26 +0400

    fixed shop display

--- navolga/navolga_main/urls.py
+++ navolga/navolga_main/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.home, name='shop_home'),
@@ -14,3 +17,5 @@ urlpatterns = [
     # path('search/', views.search_products, name='search_products'),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
